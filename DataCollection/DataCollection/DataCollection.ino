8096d72b22c06e0d801d9620939c4442630abb76
wkrhuang <714884865@qq.com>
2021-01-24 23:41:02 +0000

    update

--- DataCollection/DataCollection/DataCollection.ino
+++ DataCollection/DataCollection/DataCollection.ino
@@ -17,7 +17,6 @@ void loop() {
   
   
   if (micros() - lastRead >= READ_PERIOD) {
-
     if(flag==0){
         while(Serial.available()==0){
           delay(100);
@@ -26,11 +25,6 @@ void loop() {
       }
     
     if(flag==1){
-      delay(1000);
-      flag=2;
-    } 
-
-    if(flag==2){
       lastRead += READ_PERIOD;
       sensorV[0] = analogRead(A0);
       sensorV[1] = analogRead(A1);
@@ -38,7 +32,7 @@ void loop() {
       Serial.println(sensorV[0]);
       Serial.println(sensorV[1]);
       Serial.println(sensorV[2]);
-      Serial.println(micros());
+      //Serial.println(micros());
     }
   }
   
