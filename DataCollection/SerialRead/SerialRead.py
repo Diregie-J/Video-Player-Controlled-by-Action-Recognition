a589476d1a04b810fb55bb2aeae4d2a7a2fad3d1
J Zhang <39611884+Regnigh@users.noreply.github.com>
2021-02-01 19:19:12 +0000

    auto file naming

--- DataCollection/SerialRead/SerialRead.py
+++ DataCollection/SerialRead/SerialRead.py
@@ -5,13 +5,15 @@ from time import sleep
 import vis
 import os
 
-def decide_filename(motion: string) -> string:
+
+def decide_filename(action):
     i = 0
-    
-    while os.path.exists(os.path.join(motion, motion + str(i) + ".csv"):
+
+    while os.path.exists(os.path.join(action, action + str(i) + ".csv")):
         i += 1
-    
-    return os.path.join(motion, motion + str(i) + ".csv")
+
+    return os.path.join(action, action + str(i) + ".csv")
+
 
 # change filename, port number, and baudrate if needed
 filename = decide_filename("fist")
@@ -19,7 +21,7 @@ port = "COM9"
 baudrate = 19200
 
 ser = serial.Serial(port, baudrate)
-ser.set_buffer_size(rx_size = 2147483647, tx_size = 2147483647)
+ser.set_buffer_size(rx_size=2147483647, tx_size=2147483647)
 
 sleep(2)
 
@@ -29,24 +31,23 @@ sleep(0.01)
 
 f = open(filename, 'w')
 
-i=0
+i = 0
 ser.flushInput()
 ser.flushOutput()
 for i in range(300):
-    #time1 = time.time()
+    # time1 = time.time()
     f.write(str(ser.readline().strip().decode('utf-8')))
     f.write(',')
     f.write(str(ser.readline().strip().decode('utf-8')))
     f.write(',')
     f.write(str(ser.readline().strip().decode('utf-8')))
     f.write(',')
-    f.write('0') # label
+    f.write('0')  # label
     f.write('\n')
-
 
     f.close()
     f = open(filename, 'a')
-    #time2=time.time()-time1
-    #print(time2)
+    # time2=time.time()-time1
+    # print(time2)
 
 vis.visFile(filename)
