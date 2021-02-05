8e4d79aef75baaf2c668db601c44b81568acafc5
Yiqiang Huang <46109629+walkeronwater@users.noreply.github.com>
2021-02-05 17:16:45 +0000

    Update SerialRead.py

--- DataCollection/SerialRead/SerialRead.py
+++ DataCollection/SerialRead/SerialRead.py
@@ -19,7 +19,11 @@ def decide_filename(action: str) -> str:
 
 
 # change filename, port number, and baudrate if needed
+<<<<<<< HEAD
+filename = 'temp'+'111'+'.csv'
+=======
 filename = decide_filename("fist")
+>>>>>>> 1cda9d3aebe9941e9a1b07bf446df6b41cf6e7df
 port = "COM9"
 baudrate = 19200
 
@@ -29,11 +33,43 @@ ser.set_buffer_size(rx_size=2147483647, tx_size=2147483647)
 sleep(2)
 
 ser.write("a".encode())
-print("Start")
-sleep(0.01)
+
+
 
 f = open(filename, 'w')
 
+<<<<<<< HEAD
+
+ser.flushInput()
+ser.flushOutput()
+while 1:
+    ser.flushInput()
+    ser.flushOutput()
+    print("Start")
+    sleep(0.01)
+    i = 0
+    for i in range(300):
+        #time1 = time.time()
+        f.write(str(ser.readline().strip().decode('utf-8')))
+        f.write(',')
+        f.write(str(ser.readline().strip().decode('utf-8')))
+        f.write(',')
+        f.write(str(ser.readline().strip().decode('utf-8')))
+        f.write(',')
+        f.write('0') # label
+        f.write('\n')
+
+
+        f.close()
+        f = open(filename, 'a')
+        #time2=time.time()-time1
+        #print(time2)
+
+    print("Relax")
+    sleep(3)
+
+# vis.visFile(filename)
+=======
 i = 0
 ser.flushInput()
 ser.flushOutput()
@@ -54,3 +90,4 @@ for i in range(300):
     # print(time2)
 
 vis.visFile(filename)
+>>>>>>> 1cda9d3aebe9941e9a1b07bf446df6b41cf6e7df
