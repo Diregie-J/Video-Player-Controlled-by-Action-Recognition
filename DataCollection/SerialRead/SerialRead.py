2424034933042b762f4fc2f39b00500be84f473b
wkrhuang <714884865@qq.com>
2021-02-05 17:10:46 +0000

    Update SerialRead.py

--- DataCollection/SerialRead/SerialRead.py
+++ DataCollection/SerialRead/SerialRead.py
@@ -5,7 +5,7 @@ from time import sleep
 import vis
 
 # change filename, port number, and baudrate if needed
-filename = 'temp'+'33'+'.csv'
+filename = 'temp'+'111'+'.csv'
 port = "COM9"
 baudrate = 19200
 
@@ -15,29 +15,38 @@ ser.set_buffer_size(rx_size = 2147483647, tx_size = 2147483647)
 sleep(2)
 
 ser.write("a".encode())
-print("Start")
-sleep(0.01)
+
+
 
 f = open(filename, 'w')
 
-i=0
+
 ser.flushInput()
 ser.flushOutput()
-for i in range(300):
-    #time1 = time.time()
-    f.write(str(ser.readline().strip().decode('utf-8')))
-    f.write(',')
-    f.write(str(ser.readline().strip().decode('utf-8')))
-    f.write(',')
-    f.write(str(ser.readline().strip().decode('utf-8')))
-    f.write(',')
-    f.write('0') # label
-    f.write('\n')
+while 1:
+    ser.flushInput()
+    ser.flushOutput()
+    print("Start")
+    sleep(0.01)
+    i = 0
+    for i in range(300):
+        #time1 = time.time()
+        f.write(str(ser.readline().strip().decode('utf-8')))
+        f.write(',')
+        f.write(str(ser.readline().strip().decode('utf-8')))
+        f.write(',')
+        f.write(str(ser.readline().strip().decode('utf-8')))
+        f.write(',')
+        f.write('0') # label
+        f.write('\n')
 
 
-    f.close()
-    f = open(filename, 'a')
-    #time2=time.time()-time1
-    #print(time2)
+        f.close()
+        f = open(filename, 'a')
+        #time2=time.time()-time1
+        #print(time2)
 
-vis.visFile(filename)
+    print("Relax")
+    sleep(3)
+
+# vis.visFile(filename)
